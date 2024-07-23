aabebc821aeebd3912559378060a42c8af458694
DagoGres <dan333pro@gmai.com>
2024-07-22 20:05:44 -0500

    fix: linting warnings

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { Inter } from 'next/font/google';
 import styles from '@styles/Home.module.scss';
 import Hero from '@components/Hero';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
@@ -18,5 +18,5 @@ export default function Home() {
         <Hero />
       </main>
     </>
-  )
+  );
 }
