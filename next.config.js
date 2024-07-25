c4f39695c135fd345e80d251027fd8043f6ebee0
dan33pro <dan333pro@gmail.com>
2024-07-25 17:36:22 -0500

    feat: service para consumir del back y ajustes de consumo

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    API_URL: process.env.API_URL,
+    API_VERSION: process.env.API_VERSION,
+  },
 }
+
+const dotenv = require('dotenv');
+dotenv.config();
 
 module.exports = nextConfig
